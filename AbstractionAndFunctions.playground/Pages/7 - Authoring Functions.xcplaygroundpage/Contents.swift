06b830639ec5c657e329943d96c82e4a38a675b1
Victoria Li <VLi@lcs.on.ca>
2020-03-05 20:32:16 -0500

    test cases for rectangle

--- AbstractionAndFunctions.playground/Pages/7 - Authoring Functions.xcplaygroundpage/Contents.swift
+++ AbstractionAndFunctions.playground/Pages/7 - Authoring Functions.xcplaygroundpage/Contents.swift
@@ -1,7 +1,7 @@
 import Foundation
 
 /*:
-# Authoring Functions
+ # Authoring Functions
  
  Refer to this [list of formulas for various geometric figures](https://www.eqao.com/en/assessments/grade-9-math/assessment-docs/g9-formula-sheet-academic.pdf) (from the Grade 9 EQAO Mathematics assessment).
  
@@ -16,7 +16,7 @@ import Foundation
  * at least three of your formulas must use 𝝿
  
  Then, on this page, author the:
-
+ 
  * function definitions
  * appropriate documentation (as described on [page 6](@previous))
  * make good choices for function names, argument labels, and parameter names
@@ -26,7 +26,7 @@ import Foundation
  
  */
 //------------------------------ perimeter of rectangle-----------------------------------
-func perimeterOfRectangle(length: Double, width: Double) -> Double？{
+func perimeterOfRectangle(length: Double, width: Double) -> Double?{
     
     //ensure we have reasonable values
     guard length > 0, width > 0 else {
@@ -46,18 +46,24 @@ perimeterOfRectangle(length: -5, width: 25)
 //test case #3 length: 5, width: -25, result
 perimeterOfRectangle(length: 5, width: -25)
 
-print("The perimeter of that rectangle was \(result！) square units.")
+print("The perimeter of that rectangle was \(result) square units.")
 
 
 //------------------------------- perimeter of Parallelogram------------------------------
-func perimeterOfParallelogram(length: Double, side: Double) -> Double{
-
+func perimeterOfParallelogram(length: Double, side: Double) -> Double?{
+    
+    //ensure we have reasonable values
+    guard length > 0, side > 0 else {
+        //we have one or more bad inputs, so return a nil value
+        return nil
+    }
+    
     return 2*(length + side)
 }
 print("The perimeter of the parallelogram is \(perimeterOfParallelogram(length:10, side:20) )")
 
 //---------------------------------- area of triangle-------------------------------------
-func areaOfTriangle(length: Double, hight: Double) -> Double{
+func areaOfTriangle(length: Double, hight: Double) -> Double?{
     
     //ensure we have reasonable values
     guard length > 0, hight > 0 else {
@@ -67,11 +73,24 @@ func areaOfTriangle(length: Double, hight: Double) -> Double{
     
     return 0.5 * length * hight
 }
-print("The perimeter of the parallelogram is \(areaOfTriangle(length:5, hight: 3) )")
+
+//test case #1
+let result = areaOfTriangle(length:5, hight: 3)
+
+//test case #2
+areaOfTriangle(length:-5, hight: 3)
+
+//test case #3
+areaOfTriangle(length:5, hight: -3)
+
+//test case #4
+areaOfTriangle(length:-5, hight: -3)
+
+print("The perimeter of the parallelogram is \(result) )")
 
 //----------------------------------- area of circle---------------------------------------
 func areaOfCircle(radious: Double) -> Double{
-    return pou * radious * radious
+    return pou(radious) * radious
 }
 print("The perimeter of the parallelogram is \(areaOfTriangle(length:5, hight: 3) )")
 /*:
